e6b26518254a974de6d316c9ec5ac6c7c977055d
surfer05 <rahul05alok@gmail.com>
2022-12-19 23:55:16 +0530

    One more commit

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,7 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0xC357405bf135BD10aCD809C5DD72592C951d55D1";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-lemon-one.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
+// 0xd841697Bbc4083a8B7AF2fC463B5F4b789cBfb80
